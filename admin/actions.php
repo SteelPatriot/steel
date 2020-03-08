827a72307756fc7ae01271ef4acec3b6ed57982b
steel <steel.patriot@yandex.ru>
2020-03-08 22:57:57 +0700

    dsf

--- admin/actions.php
+++ admin/actions.php
@@ -7,15 +7,15 @@ $linc = $core->db($host, $user, $password, $database);
 
 
 if(isset($_POST["theme"])){
-    if(!empty($_POST["site_name"])&!empty($_POST["site_url"])&!empty($_POST["coment"])) {
+    if(!empty($_POST["theme_name"])&!empty($_POST["theme_url"])&!empty($_POST["coment"])) {
 
-        $site_name = $_POST["site_name"];
-        $site_url = $_POST["site_url"];
+        $theme_name = $_POST["theme_name"];
+        $theme_url = $_POST["theme_url"];
         $coment = $_POST["coment"];
 
 
         $stmt = $linc->prepare($insert_theme);
-        $stmt->bind_param('sss', $site_name, $site_url, $coment);
+        $stmt->bind_param('sss', $theme_name, $theme_url, $coment);
 
         $stmt->execute();
 
