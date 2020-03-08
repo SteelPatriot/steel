691d620c20bbf0f11fc40412cd9ba6e603b8c8cf
steel <steel.patriot@yandex.ru>
2020-03-09 02:51:29 +0700

    dsf

--- core/DataBase.php
+++ core/DataBase.php
@@ -8,7 +8,7 @@ class DataBase
     protected function connect(){
 
         $this->dataInfo = require_once "../config_core.php";
-        $dsn = "mysql:host=localhost, dbname=steel";
+        $dsn = "mysql:host=localhost; dbname=steel";
         $pdo = new PDO($dsn, "steel", "vivi123");
         $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
         return $pdo;
