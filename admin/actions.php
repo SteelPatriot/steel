405894ca1a42680ba2229cd730f280eee5702970
steel <steel.patriot@yandex.ru>
2020-03-08 22:16:47 +0700

    dsf

--- admin/actions.php
+++ admin/actions.php
@@ -1,9 +1,11 @@
 <?php
 
-if(!isset($_POST["site_name"])&!isset($_POST["site_url"])&!isset($_POST["coment"])){
+if(isset($_POST["theme"])){
+    if(!empty($_POST["site_name"])&!empty($_POST["site_url"])&!empty($_POST["coment"])) {
 
-
-    echo "true";
-
+        echo $_POST["site_name"];
+        echo $_POST["site_url"];
+        echo $_POST["coment"];
+    }
 
 }
--- admin/index.php
+++ admin/index.php
@@ -3,19 +3,10 @@
 
 echo '
 <br>
-<form action="" method="post" >
+<form action="actions.php" method="post" >
    <input type="text" name="site_name">
    <input type="text" name="site_url">
    <input type="text" name="coment">
 <input type="submit" value="Выполнить запрос" name="theme" >
 </form>' ;
 
-if(isset($_POST["theme"])){
-    if(!empty($_POST["site_name"])&!empty($_POST["site_url"])&!empty($_POST["coment"])) {
-
-        echo $_POST["site_name"];
-        echo $_POST["site_url"];
-        echo $_POST["coment"];
-    }
-
-}
