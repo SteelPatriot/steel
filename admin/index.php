721c9ec3bb27bfdd2db0b6388cdb5811adc53b9e
steel <steel.patriot@yandex.ru>
2020-03-08 21:39:41 +0700

    index

--- admin/index.php
+++ admin/index.php
@@ -3,10 +3,17 @@
 
 echo '
 <br>
-<form action="actions.php" method="post">
+<form action="" method="post">
    <input type="text" name="site_name">
    <input type="text" name="site_url">
    <input type="text" name="coment">
 <input type="submit" value="Выполнить запрос">
 </form>' ;
 
+if(!isset($_POST["site_name"])&!isset($_POST["site_url"])&!isset($_POST["coment"])){
+
+
+    echo "true";
+
+
+}
