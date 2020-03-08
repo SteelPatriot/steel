ad424dfe8fb8b939ebcfed809334a20f36a772a6
steel <steel.patriot@yandex.ru>
2020-03-08 22:05:14 +0700

    dsf

--- admin/index.php
+++ admin/index.php
@@ -3,14 +3,14 @@
 
 echo '
 <br>
-<form action="" method="post">
+<form action="" method="post" name="theme">
    <input type="text" name="site_name">
    <input type="text" name="site_url">
    <input type="text" name="coment">
-<input type="submit" value="Выполнить запрос">
+<input type="submit" value="Выполнить запрос" >
 </form>' ;
 
-if(isset($_POST["submit"])){
+if(isset($_POST["theme"])){
     if(!empty($_POST["site_name"])&!empty($_POST["site_url"])&!empty($_POST["coment"])) {
 
         echo $_POST["site_name"];
