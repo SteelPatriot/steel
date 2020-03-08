d125c84628b352f37cde825fc098d284e6bec28f
steel <steel.patriot@yandex.ru>
2020-03-08 23:12:39 +0700

    dsf

--- admin/actions.php
+++ admin/actions.php
@@ -1,20 +1,23 @@
 <?php
 require_once "../core/Core.php";
 require_once "../config_core.php";
-$insert_theme = "INSERT INTO theme VALUES (?, ?, ?)";
-$core = new Core();
-$linc = $core->db($host, $user, $password, $database);
+
 
 
 if(isset($_POST["theme"])){
     if(!empty($_POST["theme_name"])&!empty($_POST["theme_url"])&!empty($_POST["coment"])) {
+
+        //$insert_theme =
+        $core = new Core();
+        $linc = $core->db($host, $user, $password, $database);
+
 
         $theme_name = $_POST["theme_name"];
         $theme_url = $_POST["theme_url"];
         $coment = $_POST["coment"];
 
 
-        $stmt = $linc->prepare($insert_theme);
+        $stmt = $linc->prepare("INSERT INTO theme VALUES (?, ?, ?)");
         $stmt->bind_param('sss', $theme_name, $theme_url, $coment);
 
         $stmt->execute();
