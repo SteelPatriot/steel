c484c81f469e433c7eef667004c334397612984e
steel <steel.patriot@yandex.ru>
2020-03-09 00:11:23 +0700

    dsf

--- admin/actions.php
+++ admin/actions.php
@@ -12,12 +12,13 @@ if(isset($_POST["theme"])){
 
 
 
-        $stmt = $linc->prepare("INSERT INTO theme (theme_name, theme_url, theme_coment) VALUES (?, ?, ?)");
-        $stmt->bind_param('sss', $theme_name, $theme_url, $coment);
-
         $theme_name = $_POST["theme_name"];
         $theme_url = $_POST["theme_url"];
         $coment = $_POST["coment"];
+
+        $stmt = $linc->prepare("INSERT INTO theme (theme_name, theme_url, theme_coment) VALUES (?, ?, ?)");
+        $stmt->bind_param('sss', $theme_name, $theme_url, $coment);
+
         $stmt->execute();
 $linc->commit();
         $stmt->close();
