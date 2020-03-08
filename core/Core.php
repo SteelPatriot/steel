19090ca1dcb33da93f9725366e422de4dfc74f5d
steel <steel.patriot@yandex.ru>
2020-03-08 20:43:01 +0700

    index

--- core/Core.php
+++ core/Core.php
@@ -3,7 +3,6 @@
 class Core
 {
     function db ($host, $user, $password, $database){
-
         $link = mysqli_connect($host, $user, $password, $database)
         or die("Ошибка " . mysqli_error($host, $user, $password, $database));
         mysqli_close($link);
--- index.php
+++ index.php
@@ -2,6 +2,5 @@
 require_once "core/Core.php";
 require_once "config_core.php";
 $core = new Core();
-
 $linc = $core->db($host, $user, $password, $database);
 
