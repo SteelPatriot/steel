a2600bb1a3d19dc3bcd3d1bcf55a71a6fff751eb
steel <steel.patriot@yandex.ru>
2020-03-09 02:31:01 +0700

    dsf

--- core/DataBase.php
+++ core/DataBase.php
@@ -7,8 +7,8 @@ class DataBase
 
     protected function connect(){
         $this->dataInfo = require_once "../config_core.php";
-        $dsn = "mysql:host=".$this->dataInfo['host'].", dbname=".$this->dataInfo['database'].", charset=utf8";
-        $pdo = new PDO($dsn,$this->dataInfo['usensne'],$this->dataInfo['password'])  ;
+        $dsn = "mysql:host=".$this->dataInfo['host'].", dbname=".$this->dataInfo['database'].", charset=utf8 ";
+        $pdo = new PDO($dsn, $this->dataInfo['usensne'], $this->dataInfo['password'])  ;
         $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
         return $pdo;
 }
