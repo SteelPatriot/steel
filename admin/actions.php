dbc0191890e1a2b8ff880456bc0f65b619914659
steel <steel.patriot@yandex.ru>
2020-03-08 23:28:38 +0700

    dsf

--- admin/actions.php
+++ admin/actions.php
@@ -12,14 +12,14 @@ if(isset($_POST["theme"])){
         $linc = $core->db($host, $user, $password, $database);
 
 
-        $theme_name = $_POST["theme_name"];
-        $theme_url = $_POST["theme_url"];
-        $coment = $_POST["coment"];
 
 
         $stmt = $linc->prepare("INSERT INTO theme VALUES (?, ?, ?)");
         $stmt->bind_param('sss', $theme_name, $theme_url, $coment);
 
+        $theme_name = $_POST["theme_name"];
+        $theme_url = $_POST["theme_url"];
+        $coment = $_POST["coment"];
         $stmt->execute();
 
         printf("%d Row inserted.\n", $stmt->affected_rows);
