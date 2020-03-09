cd953861f52b00db85acbd2d02392f09667bffb4
steel <steel.patriot@yandex.ru>
2020-03-09 19:25:37 +0700

    dsf

--- core/index.php
+++ core/index.php
@@ -3,6 +3,6 @@ session_start();
 $_SESSION['themeName'] = 'steel';
 require_once "core/Core.php";
 $core = new Core();
-define("urlTheme", $core->deamonTheme($_SESSION['themeName']));
-echo urlTheme;
-require_once urlTheme."/theme.php";
+//define("urlTheme", $core->deamonTheme($_SESSION['themeName']));
+
+require_once "theme/steel/theme.php";
