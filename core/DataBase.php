03a190642ae1293678d19be219aff22f4967cdb1
steel <steel.patriot@yandex.ru>
2020-03-09 14:15:36 +0700

    dsf

--- core/DataBase.php
+++ core/DataBase.php
@@ -8,7 +8,7 @@ class DataBase
     protected function connect(){
 
         $this->dataInfo = require_once "../config_core.php";
-        $dsn = 'mysql:host=localhost;dbname=root;charset=utf8';
+        $dsn = 'mysql:host=localhost;dbname=steel;charset=utf8';
         $pdo = new PDO($dsn, "root", "");
         $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
         $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
