e1e36b3c15ea610d084063eaf9be17ff152a4a47
steel <steel.patriot@yandex.ru>
2020-03-09 15:07:19 +0700

    dsf

--- admin/index.php
+++ admin/index.php
@@ -4,8 +4,8 @@
 echo '
 <br>
 <form action="" method="post" >
-   <input type="text" name="site_name">
-   <input type="text" name="site_url">
+   <input type="text" name="theme_name">
+   <input type="text" name="theme_url">
    <input type="text" name="coment">
 <input type="submit" value="Выполнить запрос" name="theme" >
 </form>' ;
