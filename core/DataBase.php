e457bdfbdee03267f1f288644f47599a447ed9fd
steel <steel.patriot@yandex.ru>
2020-03-09 14:23:44 +0700

    dsf

--- core/DataBase.php
+++ core/DataBase.php
@@ -9,9 +9,11 @@ class DataBase
 
         $this->dataInfo = require_once "../config_core.php";
         $dsn = 'mysql:host=localhost;dbname=steel;charset=utf8';
-        $pdo = new PDO($dsn, "root", "");
+        $pdo = new PDO($dsn, "root", "", array(PDO::ATTR_PERSISTENT => true));
         $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
         $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
         return $pdo;
 }
 public function linc (){
